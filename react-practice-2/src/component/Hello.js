73b3e48c9ff74be9e54fe7d0aa1a4e7a8e8d9ae9
yuunseo <dbstj7457@naver.com>
2023-02-27 15:10:18 +0900

    #7. stat 시작

--- react-practice-2/src/component/Hello.js
+++ react-practice-2/src/component/Hello.js
@@ -9,22 +9,10 @@ export default Hello; */
 //두번째 방법
 export default function Hello() {
 
-    function showName(){
-        console.log("Yunseo");
-    }
-
-    function showText(e){
-        if(e.key === 'Enter'){
-            console.log(e.target.value);
-        };
-    }
-
     return(
         <div>
             <h1>Hello</h1>
-            <button onClick={showName}>Show Name</button>
-            <button onClick={()=>{console.log("21");}}>Show Age</button>
-            <input type="text" onKeyPress={showText}></input>
+            <h2>State</h2>
         </div>
         
     )
