ac398b010eaa24f7da4e71ec0ef67400d8b8b7ac
yuunseo <dbstj7457@naver.com>
2023-02-27 15:16:10 +0900

    #7. useState() 방법

--- react-practice-2/src/component/Hello.js
+++ react-practice-2/src/component/Hello.js
@@ -1,18 +1,19 @@
-import styles from './Hello.module.css'
-/*첫번재 방법
-const Hello=()=>{
-    <p>Hello</p>;
-};
+import styles from './Hello.module.css';
+import {useState} from "react";
 
-export default Hello; */
 
-//두번째 방법
 export default function Hello() {
+    const [name,setName] = useState("yunseo");
 
+    function changeName(){
+        setName(name === "yunseo" ? "KYS" : "yunseo");
+    }
     return(
         <div>
             <h1>Hello</h1>
             <h2>State</h2>
+            <p>{name}</p>
+            <button onClick={changeName}>Change</button>
         </div>
         
     )
